608a34535d6bfb8283945a02b928bf1d913fb6e7
Nicolai Czempin <nicolai.czempin@gmail.com>
2016-04-02 01:55:00 +0200

    Prepare to use lambda for letting command set cl options

--- src/main.cpp
+++ src/main.cpp
@@ -31,21 +31,59 @@ void print_usage()
       << endl;
   cout << "to read about a specific subcommand or concept." << endl;
 }
-
+void print_num(int i)
+{
+  std::cout << i << '\n';
+}
 //TODO: move to command
 struct option* long_options_()
 {
 //  int stdin_flag = 0;
 //  int stdin_paths_flag = 0;
 //  int no_filters_flag = 0;
-//  int literally_flag = 0;
-  static struct option long_options[] =
+  static struct option long_options1[] =
         { //              { "stdin", no_argument, &stdin_flag, 1 },
           //              { "stdin-paths", no_argument, &stdin_paths_flag, 1 },
           //              { "no-filters", no_argument, &no_filters_flag, 1 },
           //              { "literally", no_argument, &literally_flag, 1 },
         { "path", required_argument, 0, 0 },
         { 0, 0, 0, 0 } };
+
+  function<void(int)> f_display = print_num;
+  f_display(-9);
+  struct option furz;
+
+  // store a lambda
+  function<void(string, int, int*, bool)> fill_options =
+      [&](string name, int has_arg, int* flag, bool value) {
+        if (name == "path") {
+          furz.name = "path";
+          furz.has_arg = has_arg;
+          furz.flag = flag;
+          furz.val = value;
+        } else {
+          furz.name = "hello";
+        }
+      };
+  string xyz("testing");
+  int literally_flag = 0;
+  fill_options("path", required_argument, &literally_flag, false);
+  static struct option long_options[] =
+        { furz,
+            { 0, 0, 0, 0 } };
+  cout << "wurst" << furz.name << endl;
+
+//  function<void()> func = [] (bool required, int &flag, bool flag_state) {
+//    cout << "Hello world" << endl;
+//  };
+//
+//  string name = "path";
+//
+//  //this needs to go inside hash-object command
+//  if (name == "path") {
+//    func(true, literally_flag, false); // now call the function
+//  }
+
   return long_options;
 }
 
@@ -72,7 +110,8 @@ void handle_options(shared_ptr<Command> command, int argc, char* argv[])
     /* getopt_long stores the option index here. */
     int option_index = 0;
     string short_options_hash_object = command->getShortOptions();
-    c = getopt_long(argc, argv, short_options_hash_object.c_str(), long_options,
+    c = getopt_long(argc, argv, short_options_hash_object.c_str(),
+        long_options,
         &option_index);
     /* Detect the end of the options. */
     if (c == -1) {
